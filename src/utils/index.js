755020ea03d6ff0a0d1d2381d487aad47fe64d3d
ardanclassic <ardan.classic@gmail.com>
2022-10-20 10:53:57 +0700

    adding utils

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles({
+  root: {
+    '&.MuiDataGrid-root .MuiDataGrid-cell:focus': {
+      outline: 'none',
+    },
+  }
+});
